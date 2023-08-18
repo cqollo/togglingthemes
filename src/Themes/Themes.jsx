9bb5d3db1ea98f95456b4f82313c39de8123fd52
Cqollo <cokatch@yahoo.com>
2023-08-18 15:48:36 +0300

    tweaking the toggle button

--- src/Themes/Themes.jsx
+++ src/Themes/Themes.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Themes = (prop) => {
   return (
     <div>
-          <button onClick={prop.onclick}>Set Theme</button>
+          <button className='btn' onClick={prop.onclick}>Set Theme</button>
           <p>This is my first theme project</p>
     </div>
   )
